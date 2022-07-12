14813302f400e5adbf2c00497483fe17da9e1a69
fabioseixas <dev.fabioseixas@gmail.com>
2022-07-12 05:46:23 -0300

    first commit

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,75 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.keymap.set
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers                                        -> Trying to live without it for now
+-- keymap("n", "<S-l>", ":bnext<CR>", opts)
+-- keymap("n", "<S-h>", ":bprevious<CR>", opts)  
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Enclose in quotes
+keymap("v", "'", "c''<Esc>P", opts)
+keymap("v", "\"", "c\"\"<Esc>P", opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Terminal --
+-- Better terminal navigation
+-- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+-- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+-- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+-- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Toggle File Explorer
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+
+-- File search
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+
+-- Text search
+keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+
+-- Text search
+keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
+
+-- Buffer search
+keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+
+-- Turn off current highligh 
+keymap("n", "<leader>h", ":nohlsearch<CR>", opts)
