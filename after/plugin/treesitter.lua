1ebed9b88abb3b42ee3d093274981e47380afff4
fabioseixas <dev.fabioseixas@gmail.com>
2025-01-21 07:47:18 -0300

    disable treesitter for html

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -41,6 +41,12 @@ require 'nvim-treesitter.configs'.setup {
 
   highlight = {
     enable = true,
+    disable = function(lang, buf)
+      if lang == "html" then
+           return true
+      end
+      return false
+    end,
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
